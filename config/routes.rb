2e276e580aebba8eb910d25e8dd21a2e2e340733
“sutocha” <brassktthp@g-mail.com>
2018-01-22 17:55:44 +0900

    ルーティングの設定（%i記法を用いて）

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resource :user, only: %i(edit update)
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :users, only: %i(index edit update)
+  resources :groups, only: %i(new create edit update)
 end
