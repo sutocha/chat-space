10021a71ad7e1b5b75d757d47e6ad6b124f749e7
“sutocha” <brassktthp@g-mail.com>
2018-01-22 15:17:02 +0900

    validatesの利用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         validates :name, presence: true,
-         validates :email, presence: true, uniquness: true
+         validates :name, presence: true
+         validates :email, presence: true
+         validates :email, uniqueness: true
 end
