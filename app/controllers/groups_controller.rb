79be47cb53aaa260abadcf17e684c0adb7bc9df6
“sutocha” <brassktthp@g-mail.com>
2018-01-23 08:30:49 +0900

    グループ編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,10 +18,21 @@ class GroupsController < ApplicationController
   def edit
   end
 
-  def update
+  def
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
   end
+
   private
+
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
+  end
+
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
