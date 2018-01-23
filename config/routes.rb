f798606606a4a0b6077c6d93fc5ccba34544e4b5
“sutocha” <brassktthp@g-mail.com>
2018-01-23 09:03:29 +0900

    ルートパスの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resource :user, only: %i(edit update)
   resources :users, only: %i(index edit update)
   resources :groups, only: %i(new create edit update)
