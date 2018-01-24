cf144b8d616625a8d3376dc553440e28ca7a8345
“sutocha” <brassktthp@g-mail.com>
2018-01-24 17:11:28 +0900

    エラー修正試み時の不要な変更履歴

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,4 @@
 class GroupsController < ApplicationController
-  before_action :set_group
-
   def index
   end
 
@@ -22,21 +20,10 @@ class GroupsController < ApplicationController
   end
 
   def update
-    if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-    else
-      render :edit
-    end
   end
 
   private
-
   def group_params
-    params.require(:group).permit(:name, { user_ids: [] })
-  end
-
-
-  def set_group
-    @group = Group.find(group_params[:id])
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
