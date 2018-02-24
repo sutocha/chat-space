6091fafe9e72c6d2e00dc8df1d8432735ba6f1a2
“sutocha” <brassktthp@g-mail.com>
2018-02-24 19:34:40 +0900

    Ajaxでmessages#createを動かす

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,16 @@
 $(function(){
-  $('.form__submit').on('submit', function(){
-    console.log("メッセージを送信した。")
+  $('.form__submit').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: './messages',
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
