22ab672227e738f68341d8c1b9bb3910870500ce
“sutocha” <brassktthp@g-mail.com>
2018-03-03 14:53:34 +0900

    HTMLの追加＆エラー時の処理

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,11 +1,33 @@
 $(function(){
+  function appenedUser(user){
+    var html =`
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+      </div>
+    `
+    return html;
+  }
   $("#user-search-field").on("keyup", function(){ //テキストフィールドのクラス名を""に
     var input = $("#user-search-field").val();
     $.ajax({
       type: 'GET',
-      url: './groups/new', './groups/edit',
+      url: './groups/new',
       data: { keyword: input },
       dataType: 'json'
+    })
+    .done(function(users){
+       if (users.length !== 0) {
+         users.forEach(function(user){
+           appendUser(user);
+         });
+       }
+       else {
+         appendNoUser("一致するユーザがいません");
+       }
+    })
+    .fail(function(){
+      alert('user検索ができませんでした。');
     })
   });
 });
