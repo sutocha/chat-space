1f1810e94ffd865aaa6e6236b93518c8b56e78f0
“sutocha” <brassktthp@g-mail.com>
2018-03-08 05:24:14 +0900

    スペースの追加

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,5 +1,6 @@
 $(function(){
   var search_list = $("#user-search-result")
+
   function appendUser(user){
     console.log(user)
     var html =`
@@ -10,12 +11,14 @@ $(function(){
     `
     return html;
   }
+
   function appendNoUser(user){
     var html = `
       <div class="chat-group-user clearfix">
       </div>
     `
   }
+
   $("#user-search-field").on("keyup", function(){ //テキストフィールドのクラス名を""に
     var input = $("#user-search-field").val();
     $.ajax({
