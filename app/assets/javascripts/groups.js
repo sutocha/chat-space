a153c0a009f20c86ce907cfebfe6752f63db28a8
“sutocha” <brassktthp@g-mail.com>
2018-03-16 06:35:57 +0900

    追加ボタンの実装

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -2,6 +2,7 @@ $(function(){
   var search_list = $("#user-search-result")
 
   function appendUser(user){
+    console.log(user);
     var html =`
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
@@ -43,5 +44,21 @@ $(function(){
     .fail(function(){
       alert('user検索ができませんでした。');
     })
+  });
+
+  function addUser(user){
+    var html =`
+      <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user.id}>'
+        <input name='group[user_ids][]' type='hidden' value=${user.id}>
+        <p class='chat-group-user__name'>${user.name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>
+    `
+    return html;
+  }
+
+  $(document).on("click", ".chat-group-user__btn--add", function(){
+    $('.chat-group-user').remove();
+    $('js-add-user').append(addUser(user));
   });
 });
